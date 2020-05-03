043f67cc72b179ba4f01c6a80b73c21879ece288
matowy <mateusz.a.kalinowski@gmail.com>
2020-05-03 17:09:44 +0200

    loading data moved to package

---
+++ traininng/data.py
@@ -0,0 +1,27 @@
+from typing import Tuple
+
+import pandas as pd
+from fastai.vision import ImageList, get_transforms, ResizeMethod
+
+from traininng.train import log
+
+
+def load_data(dataset_info, image_size=300, batch_size=48, dataset_type='train') -> Tuple[ImageList, pd.DataFrame]:
+    dataset_location = dataset_info[dataset_type]['location']
+
+    log.info(f'Loading data from: {dataset_location}; image size: {image_size}; batch size: {batch_size}.')
+
+    labels = pd.read_csv(dataset_info['labels']['location'])
+
+    train_data = ImageList.from_df(labels[labels.is_test == 0], dataset_location,
+                                   cols='filename').split_by_rand_pct(.2).label_from_df(cols='class_name')
+
+    train_data = (train_data.transform(get_transforms(),
+                                       size=image_size,
+                                       resize_method=ResizeMethod.SQUISH,
+                                       padding_mode='reflection')
+                  .databunch())
+    train_data.batch_size = batch_size
+
+    log.info('Data loaded.')
+    return train_data, labels
--- traininng/train.py
+++ traininng/train.py
@@ -1,43 +1,19 @@
-from fastai.basic_train import Recorder, Learner
+from fastai.basic_train import Learner
 from fastai.callbacks import CSVLogger, SaveModelCallback
-
 from fastai.metrics import accuracy, LabelSmoothingCrossEntropy
-from model.efficient_net import EfficientNet
 
-import pandas as pd
 from config.structure import data_sources
-from fastai.vision import (get_transforms, ImageList, ResizeMethod)
-
+from model.efficient_net import EfficientNet
 from structure.recorder import CustomRecorder
 from structure.train_info import TrialInfo
+from traininng.data import load_data
 from utils.default_logging import configure_default_logging
 
 log = configure_default_logging(__name__)
 
 
-def load_data(dataset_location, labels, image_size=300, batch_size=48):
-    log.info(f'Loading data from: {dataset_location}; image size: {image_size}; batch size: {batch_size}.')
-
-    data = ImageList.from_df(labels[labels.is_test == 0], dataset_location,
-                             cols='filename').split_by_rand_pct(.2).label_from_df(cols='class_name')
-
-    data = (data.transform(get_transforms(),
-                           size=image_size,
-                           resize_method=ResizeMethod.SQUISH,
-                           padding_mode='reflection')
-            .databunch())
-    data.batch_size = batch_size
-
-    log.info('Data loaded.')
-    return data
-
-
 def main():
-    dataset_info = data_sources['stanford']
-    dataset_location = dataset_info['train']['location']
-
-    labels = pd.read_csv(dataset_info['labels']['location'])
-    data = load_data(dataset_location, labels, batch_size=48)
+    data = load_data(dataset_info=data_sources['stanford'], batch_size=48)
 
     def perform_efficient_net_training(model_name, epochs=40):
         model = EfficientNet.from_name(model_name, load_weights=True)
