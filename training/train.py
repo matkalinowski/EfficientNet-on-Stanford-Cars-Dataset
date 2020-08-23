811561c515776722e75e9c33c37ee59f8a207ca9
matowy <mateusz.a.kalinowski@gmail.com>
2020-08-23 17:54:30 +0200

    First methods implemented

--- training/train.py
+++ training/train.py
@@ -1,9 +1,21 @@
-import pytorch_lightning as pl
-from pytorch_lightning.callbacks import ModelCheckpoint
+from collections import Counter
+from functools import reduce
 
+import pytorch_lightning as pl
+import torch
+from fvcore.nn import flop_count
+from fvcore.nn.jit_handles import get_shape
+from pytorch_lightning.callbacks import ModelCheckpoint
+from thop import profile
+from thop.vision.basic_hooks import count_convNd
+
+from models.efficient_net.Swish import Swish
+from models.efficient_net.conv_2d import Conv2dStaticSamePadding
 from models.efficient_net.efficient_net import EfficientNet
 from models.efficient_net.efficient_nets import EfficientNets
+from training.cars_dataset_callback import StanfordCarsDatasetCallback
 from training.trial_info import TrialInfo
+from utils.xtils import calculate_FLOPs_scale, get_model_summary
 
 
 def perform_training(
@@ -19,12 +31,146 @@ def perform_training(
         advprop=advprop)
 
     trial_info = TrialInfo(model_info, load_weights, advprop)
+    # neptune_logger = NeptuneLogger(
+    #     project_name="matkalinowski/sandbox",
+    #     experiment_name="e0"
+    # )
 
     checkpoint = ModelCheckpoint(filepath=str(trial_info.output_folder), period=2, mode='min')
-    trainer = pl.Trainer(max_epochs=20, gpus=1, checkpoint_callback=checkpoint, fast_dev_run=True)
+    trainer = pl.Trainer(max_epochs=20, gpus=1,
+                         fast_dev_run=True,
+                         # logger=neptune_logger, save_last=True,
+                         callbacks=[(StanfordCarsDatasetCallback(trial_info))], checkpoint_callback=checkpoint)
+
+    # In every epoch, the loop methods are called in this frequency:
+    # validation_step() called every batch
+    # validation_epoch_end() called every epoch
+    # test_dataloader() is only called with .test()
     trainer.fit(model)
+
+    example_batch_input = torch.rand([1, 3, 224, 224])
+
+    custom_flops_calculations(model, example_batch_input)
+
+    # onnx_file_name = "EfficientNet_b0.onnx"
+    # torch_out = torch.onnx.export(model, example_batch_input, onnx_file_name, export_params=True)
+
+    fvcore_flops(example_batch_input, model)
+
+    calculate_FLOPs_scale(model, input_size=224, use_gpu=False, multiply_adds=False)
+
+    # example_batch_input = torch.rand([1, 3, 224, 224], requires_grad=True)
+    # with torch.autograd.profiler.profile() as prof:
+    #     model(example_batch_input)
+    # # NOTE: some columns were removed for brevity
+    # print(prof.key_averages().table(sort_by="self_cpu_time_total"))
+
     trainer.test(model)
 
 
+def prod_of_list(values):
+    return reduce(lambda x, y: x * y, values)
+
+
+def get_total_number_of_elements(input):
+    shp = get_shape(input)
+    return prod_of_list(shp)
+
+
+def fvcore_flops(example_batch_input, model):
+    def count_sigmoid(inputs, outputs):
+        n_elements = get_total_number_of_elements(inputs[0])
+
+        total_exp = n_elements
+        total_add = n_elements - 1
+        total_div = n_elements
+        sign_change = n_elements
+
+        total = total_exp + total_add + total_div + sign_change
+
+        return Counter({"sigmoid": total})
+
+    def build_flop_result(operation_name):
+        def flop_of_input_size(inputs, outputs):
+            n_elements = get_total_number_of_elements(inputs[0])
+            return Counter({operation_name: n_elements})
+
+        return flop_of_input_size
+
+    def count_adaptive_avg_pool2d(inputs, outputs):
+        shp_x = get_shape(inputs[0])
+        shp_y = get_shape(outputs[0])
+
+        kernel = map(lambda x: x // shp_y[1], shp_x[2:])
+        total_add = prod_of_list(kernel)
+        total_div = 1
+        kernel_ops = total_add + total_div
+        num_elements = prod_of_list(shp_y)
+
+        total = kernel_ops * num_elements
+
+        return Counter({"adaptive_avg_pool2d": total})
+
+    def count_rand(inputs, outputs):
+        return Counter({"rand": get_total_number_of_elements(outputs[0])})
+
+    def count_batch_norm(inputs, outputs):
+        n_elements = get_total_number_of_elements(inputs[0])
+
+        total_mult = n_elements
+        total_add = (n_elements - 1) * 2
+        total_sub = n_elements
+        total_div = n_elements
+        total_sqrt = n_elements
+
+        # z = gamma * (y - mean) / sqrt(variance + epsilon) + beta
+        total = total_mult + total_add + + total_sub + total_div + total_sqrt
+
+        return Counter({"batch_norm": total})
+
+    flop_results = flop_count(model, (example_batch_input,),
+                              supported_ops={
+                                  # 'aten::sigmoid': count_sigmoid,
+                                  # 'aten::rand': count_rand,
+                                  'aten::adaptive_avg_pool2d': count_adaptive_avg_pool2d,
+                                  # 'aten::mul': build_flop_result('mul'),
+                                  # 'aten::add': build_flop_result('add'),
+                                  # 'aten::div': build_flop_result('div'),
+                                  # 'aten::batch_norm': count_batch_norm,
+                                  # 'aten::dropout': build_flop_result('dropout'),
+                              })
+    flops = sum(flop_results[0].values())
+    print(flops)
+
+
+def custom_flops_calculations(model, example_batch_input):
+    def count_Conv2dStaticSamePadding(layer: Conv2dStaticSamePadding, x: (torch.Tensor,), y: torch.Tensor):
+        count_convNd(layer, x, y)
+
+    def count_swish(layer, x: (torch.Tensor,), y: torch.Tensor):
+        n_elements = x[0].numel()
+
+        total_exp = n_elements
+        total_add = n_elements - 1
+        total_div = n_elements
+        total_mult = n_elements
+        sign_change = n_elements
+
+        # we count activation functions using flops, but library returns macs, macs ~= flops*2
+        total_flops = (total_exp + total_add + total_div + total_mult+ sign_change) * 2
+
+        layer.total_ops += torch.DoubleTensor([int(total_flops)])
+
+    print('Custom calculations:')
+    macs, params = profile(model.cpu(), inputs=(example_batch_input,),
+                           custom_ops={
+                               Swish: count_swish,
+                               Conv2dStaticSamePadding: count_Conv2dStaticSamePadding})
+    # in billions
+    flops = macs / 2e9
+    print(f'{macs=}, {params=}, {flops=}'),
+    return example_batch_input
+
+
 if __name__ == '__main__':
-    perform_training(EfficientNets.b0)
+    perform_training(EfficientNets.b0, load_weights=False)
---
+++ utils/misc.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+# https://raw.githubusercontent.com/facebookresearch/SlowFast/0cc82440fee6e51a5807853b583be238bf26a253/slowfast/utils/misc.py
+
+
+import math
+import numpy as np
+import os
+from datetime import datetime
+import psutil
+import torch
+from fvcore.nn.activation_count import activation_count
+from fvcore.nn.flop_count import flop_count
+from matplotlib import pyplot as plt
+from torch import nn
+
+from utils.default_logging import configure_default_logging
+
+logger = configure_default_logging(__name__)
+
+
+def pack_pathway_output(cfg, frames):
+    """
+    Prepare output as a list of tensors. Each tensor corresponding to a
+    unique pathway.
+    Args:
+        frames (tensor): frames of images sampled from the video. The
+            dimension is `channel` x `num frames` x `height` x `width`.
+    Returns:
+        frame_list (list): list of tensors with the dimension of
+            `channel` x `num frames` x `height` x `width`.
+    """
+    if cfg.DATA.REVERSE_INPUT_CHANNEL:
+        frames = frames[[2, 1, 0], :, :, :]
+    if cfg.MODEL.ARCH in cfg.MODEL.SINGLE_PATHWAY_ARCH:
+        frame_list = [frames]
+    elif cfg.MODEL.ARCH in cfg.MODEL.MULTI_PATHWAY_ARCH:
+        fast_pathway = frames
+        # Perform temporal sampling from the fast pathway.
+        slow_pathway = torch.index_select(
+            frames,
+            1,
+            torch.linspace(
+                0, frames.shape[1] - 1, frames.shape[1] // cfg.SLOWFAST.ALPHA
+            ).long(),
+        )
+        frame_list = [slow_pathway, fast_pathway]
+    else:
+        raise NotImplementedError(
+            "Model arch {} is not in {}".format(
+                cfg.MODEL.ARCH,
+                cfg.MODEL.SINGLE_PATHWAY_ARCH + cfg.MODEL.MULTI_PATHWAY_ARCH,
+            )
+        )
+    return frame_list
+
+
+def check_nan_losses(loss):
+    """
+    Determine whether the loss is NaN (not a number).
+    Args:
+        loss (loss): loss to check whether is NaN.
+    """
+    if math.isnan(loss):
+        raise RuntimeError("ERROR: Got NaN losses {}".format(datetime.now()))
+
+
+def params_count(model):
+    """
+    Compute the number of parameters.
+    Args:
+        model (model): model to count the number of parameters.
+    """
+    return np.sum([p.numel() for p in model.parameters()]).item()
+
+
+def gpu_mem_usage():
+    """
+    Compute the GPU memory usage for the current device (GB).
+    """
+    mem_usage_bytes = torch.cuda.max_memory_allocated()
+    return mem_usage_bytes / 1024 ** 3
+
+
+def cpu_mem_usage():
+    """
+    Compute the system memory (RAM) usage for the current device (GB).
+    Returns:
+        usage (float): used memory (GB).
+        total (float): total memory (GB).
+    """
+    vram = psutil.virtual_memory()
+    usage = (vram.total - vram.available) / 1024 ** 3
+    total = vram.total / 1024 ** 3
+
+    return usage, total
+
+
+def _get_model_analysis_input(cfg, is_train):
+    """
+    Return a dummy input for model analysis with batch size 1. The input is
+        used for analyzing the model (counting flops and activations etc.).
+    Args:
+        cfg (CfgNode): configs. Details can be found in
+            slowfast/config/defaults.py
+        is_train (bool): if True, return the input for training. Otherwise,
+            return the input for testing.
+
+    Returns:
+        inputs: the input for model analysis.
+    """
+    rgb_dimension = 3
+    if is_train:
+        input_tensors = torch.rand(
+            rgb_dimension,
+            cfg.DATA.NUM_FRAMES,
+            cfg.DATA.TRAIN_CROP_SIZE,
+            cfg.DATA.TRAIN_CROP_SIZE,
+        )
+    else:
+        input_tensors = torch.rand(
+            rgb_dimension,
+            cfg.DATA.NUM_FRAMES,
+            cfg.DATA.TEST_CROP_SIZE,
+            cfg.DATA.TEST_CROP_SIZE,
+        )
+    model_inputs = pack_pathway_output(cfg, input_tensors)
+    for i in range(len(model_inputs)):
+        model_inputs[i] = model_inputs[i].unsqueeze(0).cuda(non_blocking=True)
+
+    # If detection is enabled, count flops for one proposal.
+    if cfg.DETECTION.ENABLE:
+        bbox = torch.tensor([[0, 0, 1.0, 0, 1.0]])
+        bbox = bbox.cuda()
+        inputs = (model_inputs, bbox)
+    else:
+        inputs = (model_inputs,)
+    return inputs
+
+
+def get_flop_stats(model, cfg, is_train):
+    """
+    Compute the gflops for the current model given the config.
+    Args:
+        model (model): model to compute the flop counts.
+        cfg (CfgNode): configs. Details can be found in
+            slowfast/config/defaults.py
+        is_train (bool): if True, compute flops for training. Otherwise,
+            compute flops for testing.
+
+    Returns:
+        float: the total number of gflops of the given model.
+    """
+    inputs = _get_model_analysis_input(cfg, is_train)
+    gflop_dict, _ = flop_count(model, inputs)
+    gflops = sum(gflop_dict.values())
+    return gflops
+
+
+def get_activation_stats(model, cfg, is_train):
+    """
+    Compute the activation count (mega) for the current model given the config.
+    Args:
+        model (model): model to compute the activation counts.
+        cfg (CfgNode): configs. Details can be found in
+            slowfast/config/defaults.py
+        is_train (bool): if True, compute activation for training. Otherwise,
+            compute activation for testing.
+
+    Returns:
+        float: the total number of activation (mega) of the given model.
+    """
+    inputs = _get_model_analysis_input(cfg, is_train)
+    activation_dict, _ = activation_count(model, inputs)
+    activation = sum(activation_dict.values())
+    return activation
+
+
+def log_model_info(model, cfg, is_train=True):
+    """
+    Log info, includes number of parameters, gpu usage and gflops.
+    Args:
+        model (model): model to log the info.
+        cfg (CfgNode): configs. Details can be found in
+            slowfast/config/defaults.py
+        is_train (bool): if True, log info for training. Otherwise,
+            log info for testing.
+    """
+    logger.info("Model:\n{}".format(model))
+    logger.info("Params: {:,}".format(params_count(model)))
+    logger.info("Mem: {:,} MB".format(gpu_mem_usage()))
+    logger.info("Flops: {:,} G".format(get_flop_stats(model, cfg, is_train)))
+    logger.info(
+        "Activations: {:,} M".format(get_activation_stats(model, cfg, is_train))
+    )
+    logger.info("nvidia-smi")
+    os.system("nvidia-smi")
+
+
+def is_eval_epoch(cfg, cur_epoch, multigrid_schedule):
+    """
+    Determine if the model should be evaluated at the current epoch.
+    Args:
+        cfg (CfgNode): configs. Details can be found in
+            slowfast/config/defaults.py
+        cur_epoch (int): current epoch.
+        multigrid_schedule (List): schedule for multigrid training.
+    """
+    if cur_epoch + 1 == cfg.SOLVER.MAX_EPOCH:
+        return True
+    if multigrid_schedule is not None:
+        prev_epoch = 0
+        for s in multigrid_schedule:
+            if cur_epoch < s[-1]:
+                period = max(
+                    (s[-1] - prev_epoch) // cfg.MULTIGRID.EVAL_FREQ + 1, 1
+                )
+                return (s[-1] - 1 - cur_epoch) % period == 0
+            prev_epoch = s[-1]
+
+    return (cur_epoch + 1) % cfg.TRAIN.EVAL_PERIOD == 0
+
+
+def plot_input(tensor, bboxes=(), texts=(), path="./tmp_vis.png"):
+    """
+    Plot the input tensor with the optional bounding box and save it to disk.
+    Args:
+        tensor (tensor): a tensor with shape of `NxCxHxW`.
+        bboxes (tuple): bounding boxes with format of [[x, y, h, w]].
+        texts (tuple): a tuple of string to plot.
+        path (str): path to the image to save to.
+    """
+    tensor = tensor - tensor.min()
+    tensor = tensor / tensor.max()
+    f, ax = plt.subplots(nrows=1, ncols=tensor.shape[0], figsize=(50, 20))
+    for i in range(tensor.shape[0]):
+        ax[i].axis("off")
+        ax[i].imshow(tensor[i].permute(1, 2, 0))
+        # ax[1][0].axis('off')
+        if bboxes is not None and len(bboxes) > i:
+            for box in bboxes[i]:
+                x1, y1, x2, y2 = box
+                ax[i].vlines(x1, y1, y2, colors="g", linestyles="solid")
+                ax[i].vlines(x2, y1, y2, colors="g", linestyles="solid")
+                ax[i].hlines(y1, x1, x2, colors="g", linestyles="solid")
+                ax[i].hlines(y2, x1, x2, colors="g", linestyles="solid")
+
+        if texts is not None and len(texts) > i:
+            ax[i].text(0, 0, texts[i])
+    f.savefig(path)
+
+
+def frozen_bn_stats(model):
+    """
+    Set all the bn layers to eval mode.
+    Args:
+        model (model): model to set bn layers to eval mode.
+    """
+    for m in model.modules():
+        if isinstance(m, nn.BatchNorm3d):
+            m.eval()
+
+
+def aggregate_sub_bn_stats(module):
+    """
+    Recursively find all SubBN modules and aggregate sub-BN stats.
+    Args:
+        module (nn.Module)
+    Returns:
+        count (int): number of SubBN module found.
+    """
+    count = 0
+    for child in module.children():
+        count += aggregate_sub_bn_stats(child)
+    return count
