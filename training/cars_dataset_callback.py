dd5ef2fb6df9078f86eca9c72ff8b15c11df1264
matowy <mateusz.a.kalinowski@gmail.com>
2020-09-03 18:22:52 +0200

    metrics logging moved to callback

--- training/cars_dataset_callback.py
+++ training/cars_dataset_callback.py
@@ -1,9 +1,13 @@
 from time import time
 
+import torch
 from pytorch_lightning import Callback
+from sklearn.metrics import classification_report
+from torch.utils.data import DataLoader
 
 from training.trial_info import TrialInfo
 from utils.default_logging import configure_default_logging
+from utils.metrics import top_k_accuracy
 from utils.misc import calculate_model_info
 
 log = configure_default_logging(__name__)
@@ -17,6 +21,32 @@ log = configure_default_logging(__name__)
 #     model(example_batch_input)
 # # NOTE: some columns were removed for brevity
 # print(prof.key_averages().table(sort_by="self_cpu_time_total"))
+
+def _predict(model, data_loader):
+    model.eval()
+
+    y_true = torch.tensor([], dtype=torch.long, device=model.device)
+    y_pred = torch.tensor([], device=model.device)
+
+    with torch.no_grad():
+        for data in data_loader:
+            inputs = [i.to(model.device) for i in data[:-1]]
+            labels = data[-1].to(model.device)
+
+            outputs = model(*inputs)
+            y_true = torch.cat((y_true, labels), 0)
+            y_pred = torch.cat((y_pred, outputs), 0)
+
+    _, y_pred_class = torch.max(y_pred, 1)
+
+    return y_true, y_pred, y_pred_class
+
+
+def _log_metrics(metrics, trainer):
+    if trainer.logger is not None:
+        for name, metric in metrics:
+            trainer.logger.experiment.log_metric(name, metric)
+            log.info(f'{name} = {metric}')
 
 
 class StanfordCarsDatasetCallback(Callback):
@@ -58,3 +88,41 @@ class StanfordCarsDatasetCallback(Callback):
         self.lap_times.append(time() - self.lap_start)
         if trainer.logger is not None:
             trainer.logger.experiment.log_metric('lap_time', self.lap_times[-1])
+
+    def calculate_metrics(self, trainer, data, prefix):
+        y_true, y_pred, y_pred_class = _predict(trainer.model, DataLoader(data, batch_size=150))
+
+        metrics = top_k_accuracy(y_pred, y_true, (1, 3, 5, 10))
+
+        pred_class_numpy = y_pred_class.cpu().numpy()
+        y_numpy = y_true.cpu().numpy()
+
+        # logs['multi_label_confusion_matrix_results'] = multilabel_confusion_matrix(y_numpy, pred_class_numpy)
+        # logs['confusion_matrix'] = plm.ConfusionMatrix()(pred_class, y)
+
+        # TODO: check this out
+        # from pytorch_lightning.metrics.functional import multiclass_precision_recall_curve
+
+        # to know more: https://towardsdatascience.com/multi-class-metrics-made-simple-part-ii-the-f1-score-ebe8b2c2ca1
+        classification_report_results = classification_report(pred_class_numpy, y_numpy, output_dict=True)
+
+        for type_of_avg in ['macro avg', 'weighted avg']:
+            averaged_results = classification_report_results[type_of_avg]
+            for metric in averaged_results.keys():
+                metrics[f"{type_of_avg.replace(' ', '_')}_{metric}"] = averaged_results[metric]
+
+        _log_metrics(metrics, trainer)
+        return metrics
+
+
+def on_training_epoch_end(self, trainer, pl_module):
+    self.calculate_metrics(trainer.model, trainer.datamodule.train_data, prefix='train')
+
+
+def on_validation_end(self, trainer, pl_module):
+    self.calculate_metrics(trainer.model, trainer.datamodule.val_data, prefix='val')
+
+
+def on_test_epoch_end(self, trainer, pl_module):
+    self.calculate_metrics(trainer.model, trainer.datamodule.test_data, prefix='test')
+
--- training/cars_dataset_lightning_module.py
+++ training/cars_dataset_lightning_module.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader
 
 from utils.default_logging import configure_default_logging
 from utils.metrics import top_k_accuracy
+from pytorch_lightning.metrics import functional as FM
 
 log = configure_default_logging(__name__)
 
@@ -19,86 +20,26 @@ class StanfordCarsDatasetLightningModule(pl.LightningModule, ABC):
         self.image_size = image_size
         self.loss = CrossEntropyLoss()
 
-    def single_step(self, batch, batch_idx, loss_type):
+    def training_step(self, batch, batch_idx):
         x, y = batch
-
         pred = self(x)
         loss = self.loss(pred, y)
-        logs = {loss_type: loss.item()}
+        return pl.TrainResult(loss)
 
-        return {loss_type: loss, 'log': logs}
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        pred = self(x)
+        loss = self.loss(pred, y)
+        acc = FM.accuracy(pred, y)
 
-    def training_step(self, train_batch, batch_idx):
-        return self.single_step(train_batch, batch_idx, 'loss')
+        result = pl.EvalResult(checkpoint_on=loss)
+        result.log_dict({'val_acc': acc, 'val_loss': loss})
+        return result
 
-    def validation_step(self, val_batch, batch_idx):
-        return self.single_step(val_batch, batch_idx, 'val_loss')
-
-    def test_step(self, test_batch, batch_idx):
-        return self.single_step(test_batch, batch_idx, 'test_loss')
-
-    def predict(self, data_loader):
-        self.eval()
-
-        y_true = torch.tensor([], dtype=torch.long, device=self.device)
-        y_pred = torch.tensor([], device=self.device)
-
-        with torch.no_grad():
-            for data in data_loader:
-                inputs = [i.to(self.device) for i in data[:-1]]
-                labels = data[-1].to(self.device)
-
-                outputs = self(*inputs)
-                y_true = torch.cat((y_true, labels), 0)
-                y_pred = torch.cat((y_pred, outputs), 0)
-
-        _, y_pred_class = torch.max(y_pred, 1)
-
-        return y_true, y_pred, y_pred_class
-
-    def calculate_metrics(self, data, loss, prefix):
-        y_true, y_pred, y_pred_class = self.predict(DataLoader(data, batch_size=150))
-
-        metrics = top_k_accuracy(y_pred, y_true, (1, 3, 5, 10))
-
-        pred_class_numpy = y_pred_class.cpu().numpy()
-        y_numpy = y_true.cpu().numpy()
-
-        # logs['multi_label_confusion_matrix_results'] = multilabel_confusion_matrix(y_numpy, pred_class_numpy)
-        # logs['confusion_matrix'] = plm.ConfusionMatrix()(pred_class, y)
-
-        # TODO: check this out
-        # from pytorch_lightning.metrics.functional import multiclass_precision_recall_curve
-
-        # to know more: https://towardsdatascience.com/multi-class-metrics-made-simple-part-ii-the-f1-score-ebe8b2c2ca1
-        classification_report_results = classification_report(pred_class_numpy, y_numpy, output_dict=True)
-
-        for type_of_avg in ['macro avg', 'weighted avg']:
-            averaged_results = classification_report_results[type_of_avg]
-            for metric in averaged_results.keys():
-                metrics[f"{type_of_avg.replace(' ', '_')}_{metric}"] = averaged_results[metric]
-
-        metrics = {f'{prefix}_{k}': v for k, v in metrics.items()}
-        metrics[f'{prefix}_loss'] = loss
-        return metrics
-
-    def training_epoch_end(self, outputs):
-        avg_loss = torch.stack([x['loss'] for x in outputs]).mean()
-        metrics = self.calculate_metrics(self.trainer.datamodule.train_data, avg_loss, prefix='train')
-
-        return {'loss': avg_loss, 'log': metrics}
-
-    def validation_epoch_end(self, outputs):
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        metrics = self.calculate_metrics(self.trainer.datamodule.val_data, avg_loss, prefix='val')
-
-        return {'val_loss': avg_loss, 'log': metrics}
-
-    def test_epoch_end(self, outputs):
-        avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
-        metrics = self.calculate_metrics(self.trainer.datamodule.test_data, avg_loss, prefix='test')
-
-        return {'test_loss': avg_loss, 'log': metrics}
+    def test_step(self, batch, batch_idx):
+        result = self.validation_step(batch, batch_idx)
+        result.rename_keys({'val_acc': 'test_acc', 'val_loss': 'test_loss'})
+        return result
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=1e-3)
