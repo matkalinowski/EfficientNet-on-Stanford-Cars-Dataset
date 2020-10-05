a03cac2e86395be1e996eccc8569a8178e7c11b7
matowy <mateusz.a.kalinowski@gmail.com>
2020-10-05 18:50:31 +0200

    pdf creation code deleted.

--- notebooks/bib2md.sh
+++
@@ -1,9 +0,0 @@
-if [ -f 5_references.md ]; then
-    rm 5_references.md
-fi
-
-echo "# References" >> 5_references.md
-
-bibtex2html -o - -s abbrv -q -nodoc references.bib >> 5_references.md
-
-ex -snc '$-4,$d|x' 5_references.md
